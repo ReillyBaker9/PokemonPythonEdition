e90e24351cb3906a3968e272649834bcd1230a74
Reilly Baker <reillybaker9@gmail.com>
2024-11-16 07:58:18 -0500

    Added Caterpie to the Pokedex

--- Pokedex.py
+++ Pokedex.py
@@ -4,3 +4,5 @@ from Moves import *
 Charmander = Pokemon("Charmander", "Fire", 39, 52, 43, 60, 50, 65, [scratch, ember, fire_fang, flamethrower])
 
 Pidgey = Pokemon("Pidgey", "Normal/Flying", 40, 45, 40, 35, 35, 56, [tackle, gust, quick_attack, wing_attack])
+
+Caterpie = Pokemon("Caterpie", "Bug", 45, 30, 35, 20, 20, 45, [tackle])
